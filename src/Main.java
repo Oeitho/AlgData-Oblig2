03d3230d0e3ff8978c5ab456a7ad502a5a071b92
Øivind <s315695@stud.hioa.no>
2018-10-05 23:44:06 +0200

    Added nullstill method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 
 public class Main {
+    private static final int ITERATIONS = 100000;
     
     public static void main(String[] args) {
+        long totalTime = 0;
+        DobbeltLenketListe<Integer> liste = new DobbeltLenketListe<>();
+        for (int i = 0; i < ITERATIONS; i++) {
+            for (int j = 0; j < 10000; j++) {
+                liste.leggInn(j);
+            }
+            long start = System.nanoTime();
+            liste.nullstill();
+            long end = System.nanoTime();
+            totalTime += end - start;
+        }
+        System.out.println("nanosekunder: " + totalTime/ITERATIONS + " ns.");
     }
     
 }
