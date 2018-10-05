53931ec5a6ffb2a293efa4fc53dc3f6c8aeb1cd1
Øivind <s315695@stud.hioa.no>
2018-10-06 00:30:05 +0200

    Added remove method

--- src/Oblig2Test.java
+++ src/Oblig2Test.java
@@ -27,7 +27,7 @@ public class Oblig2Test {
         antallFeil += oppgave6();
         antallFeil += oppgave7();
         antallFeil += oppgave8();
-        //antallFeil += oppgave9();
+        antallFeil += oppgave9();
         //antallFeil += oppgave10();
 
         if (antallFeil == 0) {
